785caf86d6df20ec7d77ca959216291601a47837
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-06-12 22:18:20 -0600

    Implemented row addition

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
-use nalgebra::{Matrix, Dim, RawStorageMut, RawStorage};
-use core::ops::{AddAssign, Mul, Index, IndexMut};
+use nalgebra::{Matrix, Dim, RawStorageMut};
+use core::ops::{AddAssign, Mul};
 
 pub struct MatrixReprOfLinEq<T,R,C,S>(pub Matrix<T,R,C,S>);
 
-/// impl<T,R,C,S> MatrixReprOfLinEq<T,R,C,S> {
-///     pub fn new(matrix: Matrix<T,R,C,S>) -> Self {
-///         MatrixReprOfLinEq(matrix)
-///     }
-/// 
-///     pub fn to_matrix(self) -> Matrix<T,R,C,S> {
-///         self.0
-///     }
-/// }
+impl<T,R,C,S> MatrixReprOfLinEq<T,R,C,S> {
+    pub fn new(matrix: Matrix<T,R,C,S>) -> Self {
+        MatrixReprOfLinEq(matrix)
+    }
+
+    pub fn to_matrix(self) -> Matrix<T,R,C,S> {
+        self.0
+    }
+}
 
 impl<T,R,C,S> MatrixReprOfLinEq<T,R,C,S>
 where
@@ -36,25 +36,32 @@ where
     }
 }
 
+impl<T,R,C,S> MatrixReprOfLinEq<T,R,C,S>
+where
+    T: Clone + AddAssign,
+    R: Dim,
+    C: Dim,
+    S: RawStorageMut<T,R,C>,
+{
+    unsafe fn row_add<'a,'b>(&'a mut self, i_1: usize, i_2: usize, factor: &'b T)
+    where
+        T: Mul<&'b T, Output=T> + 'b + AddAssign<T>,
+    {
+        let ncols = self.0.ncols();
+        for j in 0..ncols {
+            let multiplier = self.0[(i_2, j)].to_owned();
+            self.0[(i_1, j)] += multiplier * factor;
+        }
+    }
+} 
+
 // impl<T,R,C,S> MatrixReprOfLinEq<T,R,C,S>
 // where
-//     T: Mul<Output=T> + Clone,
+//     T: Clone + AddAssign,
 //     R: Dim,
 //     C: Dim,
-//     S: RawStorage<T,R,C> + RawStorageMut<T,R,C>,
+//     S: RawStorageMut<T,R,C>,
 // {
-//     unsafe fn row_add<'a>(&'a mut self, i_1: usize, i_2: usize, factor: T)
-//     where
-//         &'a mut T: AddAssign<T>,
-//     {
-//         let ncols = self.0.ncols();
-//         for j in 0..ncols {
-//             let entry = (*self.0.data.get_address_unchecked_mut(i_1, j)).clone();
-//             let prod = factor.clone() * entry;
-//             self.0.data.get_unchecked_mut(i_1, j).add_assign(prod);
-//         }
-//     }
-// 
 //     unsafe fn row_mul(&mut self, i: usize, factor: T) {
 //         let ncols = self.0.ncols();
 //         for j in 0..ncols {
@@ -62,25 +69,41 @@ where
 //         }
 //     }
 // }
-// 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use nalgebra::{Matrix, Dim, matrix};
-// 
-//     #[test]
-//     fn it_works() {
-//         let m = MatrixReprOfLinEq::new(matrix!(
-//             1usize, 2usize,
-//             3usize, 4usize,
-//         ));
-//         unsafe { m.row_xchg(0, 1) };
-//         assert_eq!(
-//             m.0,
-//             matrix!(
-//                 3usize, 4usize,
-//                 1usize, 2usize
-//             )
-//         );
-//     }
-// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use nalgebra::{Matrix, Dim, matrix};
+
+    #[test]
+    fn row_xchg_works() {
+        let mut m = MatrixReprOfLinEq::new(matrix!(
+            1usize, 2usize;
+            3usize, 4usize;
+        ));
+        unsafe { m.row_xchg(0, 1) };
+        assert_eq!(
+            m.0,
+            matrix!(
+                3usize, 4usize;
+                1usize, 2usize;
+            )
+        );
+    }
+
+    #[test]
+    fn row_add_works() {
+        let mut m = MatrixReprOfLinEq::new(matrix!(
+            1i32, 2i32;
+            3i32, 4i32;
+        ));
+        unsafe { m.row_add(0, 1, &1i32) };
+        assert_eq!(
+            m.0,
+            matrix!(
+                4i32, 6i32;
+                3i32, 4i32;
+            )
+        );
+    }
+}
